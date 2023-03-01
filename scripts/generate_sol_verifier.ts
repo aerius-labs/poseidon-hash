47d63c928a98333e3e91a96e1ef720264abeb352
Jatin Nagpal <nagpaljatin1411@gmail.com>
2023-03-01 12:49:20 +0530

    test: Add test files and package.json

---
+++ scripts/generate_sol_verifier.ts
@@ -0,0 +1,3 @@
+// TODO: Complete this
+
+import { acir_read_bytes } from '@noir-lang/noir_wasm'
---
+++ test/poseidon.test.ts
@@ -0,0 +1,4 @@
+// TODO: Make tests work
+
+import { compile, acir_read_bytes } from '@noir-lang/noir_wasm';
+import { serialise_public_inputs } from '@noir-lang/aztec_backend';
